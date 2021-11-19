e90db67505d383728fe8c6112268cf84f0f4c033
Ngô Nhân Khả - 18120403 <ngonhankha2000@users.noreply.github.com>
2021-11-19 10:07:50 +0700

    Render product-detail with Database (no related-product)

--- src/app/controllers/ProductServices.js
+++ src/app/controllers/ProductServices.js
@@ -1,5 +1,29 @@
-const {models} = require('../../config/db');
+const { models } = require('../../config/db')
 
-exports.list = () => {
-    return models.books.findAll({raw:true});
+class ProductServices {
+    getAllBooks = () => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const books = models.books.findAll({ raw: true })
+                resolve(books)
+            }
+            catch (err) {
+                reject(err)
+            }
+        })
+    }
+
+    getBookByID = (ID) => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const book = models.books.findByPk(ID, { raw: true })
+                resolve(book)
+            }
+            catch (err) {
+                reject(err)
+            }
+        })
+    }
 }
+
+module.exports = new ProductServices
--- src/app/controllers/ProductsController.js
+++ src/app/controllers/ProductsController.js
@@ -3,13 +3,19 @@ const productServices = require('./ProductServices')
 class ProductsController {
     // [GET]  /products-list
     async shop(req, res) {
-        const books = await productServices.list();
+        const books = await productServices.getAllBooks();
         res.render('products/products-list', {books})
     }
 
     // [GET] /products-list/{product-detail}
-    detail(req, res) {
-        res.render('products/product-detail')
+    async detail(req, res) {
+        const bookByID = await productServices.getBookByID(req.query.ID)
+        if (bookByID==null) {
+            res.render('errors/404')
+        }
+        else {
+            res.render('products/product-detail', {bookByID})
+        }
     }
 }
 
